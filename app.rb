77c44f3da92e161a6dd0160ddea3814257a83955
mackacavs <mackenziegdixon@yahoo.com>
2019-09-02 15:10:28 +0100

    HTML IMAGE

--- app.rb
+++ app.rb
@@ -2,12 +2,19 @@ require 'sinatra'
 set :session_secret, 'super secret'
 
 get '/' do
-  "Hello Wof"
+  "Hello Wofss"
 end
 
 
 get '/secret' do
   "You stink"
 end
+
+get '/cat' do
+  "<div>
+    <img style='border: 3px dashed red' src='http://bit.ly/1eze8aE'>
+   </div>"
+end
+
 
 
